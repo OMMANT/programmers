d96a01d4288cda7e49b3cea811538174f7f68c7b
OMMANT <ommant@naver.com>
2021-05-12 20:49:53 +0900

    Modify 순위_검색.py But not solved

--- 2021 KAKAO BLIND RECRUITMENT/순위_검색.py
+++ 2021 KAKAO BLIND RECRUITMENT/순위_검색.py
@@ -1,38 +1,48 @@
-import numpy as np
+from itertools import combinations
 
 def solution(info, query):
-    data = []
+    data = {}
     answer = []
+    combination = []
 
+    for i in range(1, 5):            
+        combination += list(combinations(range(4), i))
+
+    # Add Data
     for datum in info:
         datum = datum.split()
-        datum[-1] = int(datum[-1])
-        datum = np.array(datum)
-        data.append(datum)
-    data = np.array(data)
+        score = datum.pop()
+        d = ''.join(datum)
+        try:
+            data[d].append(int(score))
+        except KeyError:
+            data[d] = [int(score)]
+        
+        for c in combination:
+            dat = datum.copy()
+            for i in c:
+                dat[i] = '-'
+            dat = ''.join(dat)
+            try:
+                data[dat].append(int(score))
+            except KeyError:
+                data[dat] = [int(score)]
     
     for q in query:
-        q = q.split(' and ')
-        temp = q[-1].split()   
-        del q[-1]
-        q = np.array(q + temp)
-        
-        mask = np.array([True] * data.shape[0])
-        for i in range(4):
-            if q[i] == '-':
-                pass
-            else:
-                mask = mask & (q[i] == data[:, i])
-        if q[4] == '-':
-            pass
-        else:
-            q[4] = int(q[4])
-            mask = mask & (int(q[4]) <= data[:, 4].astype(np.int))
-        answer.append(len(data[mask]))
+        q = q.replace('and', '').replace('  ', ' ').split()
+        score = int(q.pop())
+        count = 0
+        q = ''.join(q)
+        try:
+            for scores in data[q]:
+                if scores >= score:
+                    count += 1
+            answer.append(count)
+        except KeyError:
+            answer.append(0)
 
     return answer
         
 
-print(solution(
-    ['java backend junior pizza 150', 'python frontend senior chicken 210', 'python frontend senior chicken 150', 'cpp backend senior pizza 260', 'java backend junior chicken 80', 'python backend senior chicken 50'],
-    ['java and backend and junior and pizza 100', 'python and frontend and senior and chicken 200', 'cpp and - and senior and pizza 250', '- and backend and senior and - 150', '- and - and - and chicken 100', '- and - and - and - 150']))
+print(solution(["java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"],
+["java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"]))
